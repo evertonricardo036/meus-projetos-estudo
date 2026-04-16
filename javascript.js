226da6d6cec15a68cafa65fd1de828416cb1c607
Everton Santos <evertonricardo036@gmail.com>
2026-04-16 20:17:27 -0300

    feat: implement isJumping function to check digit difference

---
+++ javascript.js
@@ -0,0 +1,115 @@
+// crie 2 variáreis:
+// com seu nome 
+// outra com sua idade
+
+// Depois imprimir no console assim:
+
+// Meu nome é Everton e tenho 30 anos
+
+//const nome = "Everton";
+//let idade = 30;
+
+//console.log('Meu nome é ' + nome + ' e tenho ' + idade + ' anos');
+
+//const idade1 = 30;
+//const idade2 = 10;
+
+//const somaIdades = idade1 + idade2;
+
+//console.log('Eu tenho ' + idade1 + ' anos e daqui a ' + idade2 + ' anos terei ' + somaIdades);
+
+//const idade = 16;
+//const podeDirigir = idade >= 18;
+
+//console.log('Pode dirigir: ' + podeDirigir);
+
+//const idade = 20;
+//const maiorIdade = idade >= 18;
+
+//console.log('Maior de idade: ' + maiorIdade);
+
+// Exemplo if/else
+//const idade = 16;
+
+//if (idade >= 18) {
+    //console.log('Pode dirigir');
+//} else {
+    //console.log('Não pode dirigir');
+//}
+
+//const idade = 20;
+
+//if (idade >= 18) {
+    //console.log('Acesso permito');
+//} else {
+    //console.log('Acesso negado');
+//}
+
+//const idade = 15;
+
+//if (idade >= 18) {
+   // console.log('Acesso permitido');
+//} else {
+    //console.log('Acesso negado');
+//}
+
+//function saudacao () {
+    //console.log('Olá Everton ');
+//}
+
+//saudacao();
+//const nome = 'Everton';
+//let idade = 30;
+
+//function saudacao(nome, idade) {
+    //console.log('Ola ' + nome + ', você tem' + idade + ' anos');
+//}
+
+//saudacao( nome, idade);
+
+//function mostrarNumero(n) {
+    //console.log('O número é:' + n)
+//}
+
+//mostrarNumero(10);
+
+//function multiplicacao(a, b) {
+   //console.log('Resultado: ' + (a * b));
+//}
+
+//multiplicacao(25, 2);
+ 
+//function soma(a, b) {
+    //console.log('Resultado: ' + (a + b));
+//}
+
+//soma(10, 5);
+
+//function multiplicar(a, b) {
+   // console.log('Resultado: ' + (a * b));
+//}
+
+//multiplicar(5, 4);
+
+//Assunto Arrays
+
+//const valores = [5, 15, 25];
+
+//console.log('Primeiro valor: ' + valores [0]);
+
+//const valores = [5, 15, 25];
+
+//console.log('Segundo valor: ' + valores[;1]);
+
+//const valores = [5, 15, 25];
+//for (let i = 0; i < valores.length; i++) {
+   // console.log('Valor: ' + valores[i]);
+//}
+const valores = [5, 15, 25];
+for  (let i = 0; i < valores.length; i++) {
+    if (valores[i] > 10) {
+        console.log(valores[i]);
+    }
+}
+
+
